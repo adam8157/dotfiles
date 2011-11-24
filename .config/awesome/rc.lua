f2f5383a6356401a656a83b9d06609c4a2b75667
Adam Lee <adam8157@gmail.com>
2011-11-24 20:50:52 +0800

    sync awesome config with 3.4.11 default version

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -7,16 +7,41 @@ require("beautiful")
 -- Notification library
 require("naughty")
 
--- {{{ Variable definitions
--- Themes define colours, icons, and wallpapers
-beautiful.init(awful.util.getdir("config") .. "/theme.lua")
-
 -- Private naughty config
 naughty.config.default_preset.font             = "sans 13.5"
 naughty.config.default_preset.position         = "bottom_right"
 naughty.config.default_preset.fg               = beautiful.fg_focus
 naughty.config.default_preset.bg               = beautiful.bg_focus
 naughty.config.default_preset.border_color     = beautiful.border_focus
+
+-- {{{ Error handling
+-- Check if awesome encountered an error during startup and fell back to
+-- another config (This code will only ever execute for the fallback config)
+if awesome.startup_errors then
+    naughty.notify({ preset = naughty.config.presets.critical,
+                     title = "Oops, there were errors during startup!",
+                     text = awesome.startup_errors })
+end
+
+-- Handle runtime errors after startup
+do
+    local in_error = false
+    awesome.add_signal("debug::error", function (err)
+        -- Make sure we don't go into an endless error loop
+        if in_error then return end
+        in_error = true
+
+        naughty.notify({ preset = naughty.config.presets.critical,
+                         title = "Oops, an error happened!",
+                         text = err })
+        in_error = false
+    end)
+end
+-- }}}
+
+-- {{{ Variable definitions
+-- Themes define colours, icons, and wallpapers
+beautiful.init(awful.util.getdir("config") .. "/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "xterm"
