16df12ea1f4dbe6b1f2d1793475308db07a3fc88
Adam Lee <adam8157@gmail.com>
2013-01-04 19:15:47 +0800

    bigger font sizes

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -16,7 +16,7 @@ beautiful.init(awful.util.getdir("config") .. "/theme.lua")
 
 -- Private naughty config
 naughty.config.default_preset.timeout          = 3
-naughty.config.default_preset.font             = "sans 13.5"
+naughty.config.default_preset.font             = "sans 14"
 naughty.config.default_preset.position         = "bottom_right"
 naughty.config.default_preset.fg               = beautiful.fg_focus
 naughty.config.default_preset.bg               = beautiful.bg_focus
