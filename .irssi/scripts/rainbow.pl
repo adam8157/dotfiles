2db5bbf7bfcf05cbcd45bab9c18185b6327ba60a
Adam Lee <adam8157@gmail.com>
2015-08-18 13:49:09 +0800

    irssi: scripts: upgrade rainbow.pl and scriptassist.pl

--- .irssi/scripts/rainbow.pl
+++ .irssi/scripts/rainbow.pl
@@ -1,78 +1,73 @@
 #!/usr/bin/perl -w
 
-# for Irssi 0.7.98.4 and newer
-# Written by Jakub Jankowski <shasta@atn.pl>
-# And Adam Lee <adam8157#gmail.com> (since v1.5)
-
 # USAGE:
 #
-# /rsay <text>
-#  - same as /say, but outputs a colored text
+# /RSAY <text>
+#  - same as /say, but outputs a coloured text
 #
-# /rme <text>
-#  - same as /me, but outputs a colored text
+# /RME <text>
+#  - same as /me, but outputs a coloured text
 #
-# /rtopic <text>
-#  - same as /topic, but outputs a colored text
+# /RTOPIC <text>
+#  - same as /topic, but outputs a coloured text :)
 #
-# /rkick <nick> [reason]
-#  - kicks nick from the current channel with colored reason
+# /RKICK <nick> [reason]
+#  - kicks nick from the current channel with coloured reason
+#
+# /RKNOCKOUT [time] <nicks> [reason]
+#  - knockouts nicks from the current channel with coloured reason for time
 
-# CHANGES:
-#
-# 2002.01.25: Initial release (v1.0)
-# 2002.01.26: /rtopic added (v1.1)
-# 2002.01.29: /rsay works with dcc chats now (v1.2)
-# 2002.02.02: make_colors() doesn't assign any color to spaces (v1.3)
-# 2002.02.23: /rkick added(v1.4)
-# 2011.09.16: add utf8 support(v1.5)
+# Written by Jakub Jankowski <shasta@atn.pl>
+# for Irssi 0.7.98.4 and newer
 
 use strict;
 use vars qw($VERSION %IRSSI);
-use Encode qw(_utf8_on _utf8_off);
 
-$VERSION = "1.5";
+$VERSION = "1.6";
 %IRSSI = (
-	authors     => 'Jakub Jankowski, Adam Lee',
-	name        => 'rainbow',
-	description => 'Prints colored text.',
-	license     => 'GNU GPLv2 or later',
+    authors     => 'Jakub Jankowski',
+    contact     => 'shasta@atn.pl',
+    name        => 'rainbow',
+    description => 'Prints colored text. Rather simple than sophisticated.',
+    license     => 'GNU GPLv2 or later',
+    url         => 'http://irssi.atn.pl/',
 );
 
 use Irssi;
 use Irssi::Irc;
+use Encode;
 
 # colors list
+#  0 == white
 #  4 == light red
 #  8 == yellow
 #  9 == light green
 # 11 == light cyan
 # 12 == light blue
 # 13 == light magenta
-my @colors = ('4', '8', '9', '11', '12', '13');
+my @colors = ('0', '4', '8', '9', '11', '12', '13');
 
 # str make_colors($string)
-# returns random-colored string
+# returns random-coloured string
 sub make_colors {
-	my $oldstr = join '',@_;
+	my ($string) = @_;
+	Encode::_utf8_on($string);
 	my $newstr = "";
 	my $last = 255;
 	my $color = 0;
 
-	_utf8_on($oldstr);
-	@_ = split //,$oldstr;
-	foreach(@_) {
-		if ($_ eq ' ') {
-			$newstr .= $_;
+	for (my $c = 0; $c < length($string); $c++) {
+		my $char = substr($string, $c, 1);
+		if ($char eq ' ') {
+			$newstr .= $char;
 			next;
 		}
 		while (($color = int(rand(scalar(@colors)))) == $last) {};
 		$last = $color;
 		$newstr .= "\003";
 		$newstr .= sprintf("%02d", $colors[$color]);
-		$newstr .= sprintf "$_";
+		$newstr .= (($char eq ",") ? ",," : $char);
 	}
-	_utf8_off($oldstr);
 
 	return $newstr;
 }
@@ -142,7 +137,37 @@ sub rkick {
 	}
 }
 
+# void rknockout($text, $server, $destination)
+# handles /rknockout
+sub rknockout {
+    my ($text, $server, $dest) = @_;
+
+    if (!$server || !$server->{connected}) {
+        Irssi::print("Not connected to server");
+        return;
+    }
+
+    if ($dest && $dest->{type} eq "CHANNEL") {
+        my ($time, $nick, $reason) = split(/ +/, $text, 3);
+        ($time, $nick, $reason) = (300, $time, $nick . " " . $reason) if ($time !~ m/^\d+$/);
+        return unless $nick;
+        $reason = "See you in " . $time . " seconds!" if ($reason =~ /^[\ ]*$/);
+        $dest->command("/knockout " . $time . " " . $nick . " " . make_colors($reason));
+    }
+}
+
 Irssi::command_bind("rsay", "rsay");
 Irssi::command_bind("rtopic", "rtopic");
 Irssi::command_bind("rme", "rme");
 Irssi::command_bind("rkick", "rkick");
+Irssi::command_bind("rknockout", "rknockout");
+
+# changes:
+#
+# 25.01.2002: Initial release (v1.0)
+# 26.01.2002: /rtopic added (v1.1)
+# 29.01.2002: /rsay works with dcc chats now (v1.2)
+# 02.02.2002: make_colors() doesn't assign any color to spaces (v1.3)
+# 23.02.2002: /rkick added
+# 26.11.2014: utf-8 support
+# 01.12.2014: /rknockout added (v1.6)
